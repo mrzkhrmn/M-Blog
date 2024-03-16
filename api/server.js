63a83cc07e623813326d6fdcc88b752aed7c2765
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-03-16 04:33:47 +0300

    create and connect database

--- api/server.js
+++ api/server.js
@@ -1,4 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongodb is connected"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
