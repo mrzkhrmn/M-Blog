4d562b4268842ebfda46c7d47e521d41fdfffa28
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-03-18 04:25:12 +0300

    add signup funtionalty to the page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,49 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 export const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleFormDataChange(e) {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    setErrorMessage(null);
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.error) {
+        console.log(data.error);
+        return setErrorMessage(data.error);
+      }
+      if (res.ok) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="flex flex-col max-w-md mx-auto justify-center border shadow-lg py-6 mt-20 rounded-lg">
-      <div className="flex flex-col gap-6 px-20">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 px-20">
         <div className="flex flex-col">
           <label htmlFor="username">Your username</label>
           <input
@@ -11,6 +51,7 @@ export const Signup = () => {
             type="text"
             placeholder="Username..."
             className="py-2 px-4 bg-transparent border rounded-full w-full outline-none"
+            onChange={handleFormDataChange}
           />
         </div>
         <div className="flex flex-col">
@@ -20,27 +61,45 @@ export const Signup = () => {
             type="email"
             placeholder="Email..."
             className="py-2 px-4 bg-transparent border rounded-full outline-none"
+            onChange={handleFormDataChange}
           />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col relative">
           <label htmlFor="email">Your password</label>
           <input
             id="password"
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Password..."
             className="py-2 px-4 bg-transparent border rounded-full outline-none"
+            onChange={handleFormDataChange}
           />
+          <button
+            className="absolute right-8 top-9"
+            onClick={() => setShowPassword((showPassword) => !showPassword)}
+            type="button"
+          >
+            {showPassword ? (
+              <FaRegEye className="absolute text-lg" />
+            ) : (
+              <FaRegEyeSlash className="absolute text-lg" />
+            )}
+          </button>
         </div>
-      </div>
-      <div className="px-20 flex flex-col gap-4">
-        <button className="bg-blue-400 text-white text-lg w-full mt-6 py-2 rounded-lg hover:bg-blue-500">
-          Signup
-        </button>
-        <p>
-          You already have an account?{" "}
-          <Link className="text-blue-500 hover:underline">Sign in</Link>
-        </p>
-      </div>
+        <div className="flex flex-col gap-3">
+          <button
+            type="submit"
+            className="bg-blue-400 text-white text-lg w-full mt-6 py-2 rounded-lg hover:bg-blue-500"
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Signup"}
+          </button>
+          <p>
+            You already have an account?{" "}
+            <Link className="text-blue-500 hover:underline">Sign in</Link>
+          </p>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </div>
+      </form>
     </div>
   );
 };
